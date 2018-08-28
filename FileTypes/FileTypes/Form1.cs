4b3b8ad315ae4cb4efe806cfc127cade9c6b6f6e
Samantha <Samantha15perez@gmail.com>
2018-08-28 19:12:56 -0400

    added NuGet packages

--- FileTypes/FileTypes/Form1.cs
+++ FileTypes/FileTypes/Form1.cs
@@ -16,5 +16,10 @@ namespace FileTypes
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
     }
 }
